1203ecd300259889084c4a7414c36c9e16a405c8
bae-sh <hyeon1293@gmail.com>
2022-02-12 18:50:33 +0900

    ts

--- styledcomponent/src/App.tsx
+++ styledcomponent/src/App.tsx
@@ -1,11 +1,16 @@
-import Circle from "./Circle";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+`;
 function App() {
-    return (
-        <div>
-            <Circle bgColor="teal" />
-            <Circle bgColor="tomato" />
-        </div>
-    );
+    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+        const {
+            currentTarget: { value },
+        } = event;
+    };
+    return <div></div>;
 }
 
 export default App;
