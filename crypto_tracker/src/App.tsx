e73b5865b959beee127de91664aa2922ac7cea4e
bae-sh <hyeon1293@gmail.com>
2022-02-17 23:56:19 +0900

    #4.10 React Query part Two

--- crypto_tracker/src/App.tsx
+++ crypto_tracker/src/App.tsx
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import Router from "./Router";
-
+import { ReactQueryDevtools } from "react-query/devtools";
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 html, body, div, span, applet, object, iframe,
@@ -54,9 +54,11 @@ table {
   box-sizing: border-box;
 }
 body {
+    font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
   background-color:${(props) => props.theme.bgColor};
-  color:${(props) => props.theme.textColor}
+  color:${(props) => props.theme.textColor};
+  line-height: 1.2;
 }
 a {
   text-decoration:none;
@@ -69,6 +71,7 @@ function App() {
         <>
             <GlobalStyle />
             <Router />
+            <ReactQueryDevtools initialIsOpen={true} />
         </>
     );
 }
--- crypto_tracker/src/routes/Coins.tsx
+++ crypto_tracker/src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useQuery } from "react-query";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -23,9 +25,10 @@ const Coin = styled.li`
     border-radius: 15px;
     margin-bottom: 10px;
     a {
+        display: flex;
+        align-items: center;
         padding: 20px;
         transition: color 0.2s ease-in;
-        display: block;
     }
     :hover {
         a {
@@ -42,7 +45,13 @@ const Loader = styled.span`
     text-align: center;
     display: block;
 `;
-interface CoinInterface {
+const Img = styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
+`;
+
+interface ICoin {
     id: string;
     name: string;
     symbol: string;
@@ -51,32 +60,31 @@ interface CoinInterface {
     is_active: boolean;
     type: string;
 }
-const a = { a: 1 };
+
 function Coins() {
-    const [coins, setCoins] = useState<CoinInterface[]>([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        (async () => {
-            const response = await fetch(
-                "https://api.coinpaprika.com/v1/coins"
-            );
-            const json = await response.json();
-            setCoins(json.slice(0, 100));
-            setLoading(false);
-        })();
-    }, []);
+    const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
     return (
         <Container>
             <Header>
                 <Title>코인</Title>
             </Header>
-            {loading ? (
+            {isLoading ? (
                 <Loader>Loading...</Loader>
             ) : (
                 <CoinsList>
-                    {coins.map((coin) => (
+                    {data?.splice(0, 100).map((coin) => (
                         <Coin key={coin.id}>
-                            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                            <Link
+                                to={{
+                                    pathname: `/${coin.id}`,
+                                }}
+                                state={{ name: coin.name }}
+                            >
+                                <Img
+                                    src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                                />
+                                {coin.name} &rarr;
+                            </Link>
                         </Coin>
                     ))}
                 </CoinsList>
