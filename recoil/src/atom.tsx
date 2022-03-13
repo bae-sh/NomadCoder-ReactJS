de29cd15f52ddebcfc504deae0f5be8f52bdb927
bae-sh <hyeon1293@gmail.com>
2022-03-13 13:28:09 +0900

    Selectors part Two

--- recoil/src/atom.tsx
+++ recoil/src/atom.tsx
@@ -4,6 +4,10 @@ export interface IToDo {
     id: number;
     category: "TO_DO" | "DOING" | "DONE";
 }
+export const categoryState = atom({
+    key: "category",
+    default: "TO_DO",
+});
 export const toDoState = atom<IToDo[]>({
     key: "toDo",
     default: [],
@@ -12,10 +16,7 @@ export const toDoSelector = selector({
     key: "toDoSelector",
     get: ({ get }) => {
         const toDos = get(toDoState);
-        return [
-            toDos.filter((toDo) => toDo.category === "TO_DO"),
-            toDos.filter((toDo) => toDo.category === "DOING"),
-            toDos.filter((toDo) => toDo.category === "DONE"),
-        ];
+        const category = get(categoryState);
+        return toDos.filter((toDo) => toDo.category === category);
     },
 });
