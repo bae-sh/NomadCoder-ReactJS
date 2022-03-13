d5677a9910fdf17db62ef65dd3ce756ea7a391ec
bae-sh <hyeon1293@gmail.com>
2022-03-13 23:45:06 +0900

    react.memo

---
+++ trello/src/App.tsx
@@ -0,0 +1,81 @@
+import {
+    DragDropContext,
+    Droppable,
+    Draggable,
+    DropResult,
+} from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import { toDoState } from "./atoms";
+import DraggableCard from "./components/DraggableCard";
+
+const Wrapper = styled.div`
+    display: flex;
+    max-width: 480px;
+    width: 100%;
+    margin: 0 auto;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+
+const Boards = styled.div`
+    display: grid;
+    width: 100%;
+    grid-template-columns: repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+    padding: 20px 10px;
+    padding-top: 30px;
+    background-color: ${(props) => props.theme.boardColor};
+    border-radius: 5px;
+    min-height: 200px;
+`;
+
+const Card = styled.div`
+    border-radius: 5px;
+    margin-bottom: 5px;
+    padding: 10px 10px;
+    background-color: ${(props) => props.theme.cardColor};
+`;
+
+function App() {
+    const [toDos, setToDos] = useRecoilState(toDoState);
+    const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+        if (!destination) return;
+        setToDos((oldToDos) => {
+            const toDosCopy = [...oldToDos];
+            toDosCopy.splice(source.index, 1);
+            toDosCopy.splice(destination?.index, 0, draggableId);
+            return toDosCopy;
+        });
+    };
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Wrapper>
+                <Boards>
+                    <Droppable droppableId="one">
+                        {(magic) => (
+                            <Board
+                                ref={magic.innerRef}
+                                {...magic.droppableProps}
+                            >
+                                {toDos.map((toDo, index) => (
+                                    <DraggableCard
+                                        toDo={toDo}
+                                        key={toDo}
+                                        index={index}
+                                    />
+                                ))}
+                                {magic.placeholder}
+                            </Board>
+                        )}
+                    </Droppable>
+                </Boards>
+            </Wrapper>
+        </DragDropContext>
+    );
+}
+
+export default App;
