baa4fa99f0776fdc4230912aba0998845dda64de
bae-sh <hyeon1293@gmail.com>
2022-03-31 09:21:05 +0900

    #7.13 Slider part Two

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -24,12 +24,12 @@ const Box = styled(motion.div)`
     font-size: 28px;
 `;
 const box = {
-    invisible: {
-        x: 500,
+    entry: (isBack: boolean) => ({
+        x: isBack ? -500 : 500,
         opacity: 0,
         scale: 0,
-    },
-    visible: {
+    }),
+    center: {
         x: 0,
         opacity: 1,
         scale: 1,
@@ -38,30 +38,37 @@ const box = {
             duration: 1,
         },
     },
-    exit: { x: -500, opacity: 0, scale: 0, transition: { duration: 1 } },
+    exit: (isBack: boolean) => ({
+        x: isBack ? 500 : -500,
+        opacity: 0,
+        scale: 0,
+        transition: { duration: 1 },
+    }),
 };
 function App() {
     const [visible, setVisible] = useState(1);
-    const nextPlease = () =>
+    const [isBack, setIsBack] = useState(false);
+    const nextPlease = () => {
+        setIsBack(false);
         setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-    const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+    };
+    const prevPlease = () => {
+        setIsBack(true);
+        setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+    };
 
     return (
         <Wrapper>
-            <AnimatePresence>
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
-                    i === visible ? (
-                        <Box
-                            variants={box}
-                            initial="invisible"
-                            animate="visible"
-                            exit="exit"
-                            key={i}
-                        >
-                            {i}
-                        </Box>
-                    ) : null
-                )}
+            <AnimatePresence custom={isBack}>
+                <Box
+                    variants={box}
+                    initial="entry"
+                    animate="center"
+                    exit="exit"
+                    key={visible}
+                >
+                    {visible}
+                </Box>
             </AnimatePresence>
             <button onClick={nextPlease}>next</button>
             <button onClick={prevPlease}>prev</button>
