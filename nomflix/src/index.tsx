a916c21cfc6d14b3d8fce4fb93aed9f59de7ef10
bae-sh <hyeon1293@gmail.com>
2022-07-01 21:57:21 +0900

    #8.5 Home Screen part One

--- nomflix/src/index.tsx
+++ nomflix/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
 import { theme } from './theme';
@@ -72,13 +73,17 @@ a {
 `;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const client = new QueryClient();
+
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <QueryClientProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>,
 );
